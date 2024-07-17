b4eb7bb60e1ad5a2520e09b72def67cb3b5e9bd8
Ace-h121 <chathaway121@gmail.com>
2024-07-17 04:06:23 -0400

    adding command args

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"image"
-	_ "image/png"
-	_ "image/jpeg"
-	_ "image/gif"
 	"image/color"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
 	"os"
+	"strconv"
 )
 
 
@@ -37,14 +38,47 @@ func avgPixel(img image.Image, x, y, w, h int) int {
 	return sum / cnt
 }
 func main(){
-	img, err := loadImage("cat.png")
+	Args := os.Args
+
+
+	if Args[1]== "help"{
+		fmt.Println(`Usage: tiv <image_path> <scale_y> <scale_x>
+
+Arguments:
+  <image_path>   The path to the image file to be converted to ASCII art.
+  <scale_y>      The vertical scale factor. Must be an integer.
+  <scale_x>      The horizontal scale factor. Must be an integer.
+
+Description:
+  This program converts an image to ASCII art. It requires three arguments:
+  1. The path to the image file to be converted.
+  2. The vertical scale factor (scale_y) which determines the number of image pixels to be averaged for each ASCII character in the vertical direction.
+  3. The horizontal scale factor (scale_x) which determines the number of image pixels to be averaged for each ASCII character in the horizontal direction.`)
+	}
+
+	if len(Args) < 3 {
+		fmt.Println("Missing Args, please type tiv help if you need to see usage")
+		os.Exit(1)
+	}
+
+
+
+	scaleX, err := strconv.Atoi(Args[3])
+	scaleY, err := strconv.Atoi(Args[2])
+
+	if err !=nil{
+		fmt.Println("X or Y scale not valid, please enter integers")
+		os.Exit(1)
+	}
+
+	
+	img, err := loadImage(Args[1])
 	if err != nil{
 		panic(err)
 	}
 	ramp := `$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^'.`
 
 	max := img.Bounds().Max
-	scaleX, scaleY := 50, 25
 	for y := 0; y<max.Y; y+= scaleX{
 		for x := 0; x<max.X; x +=scaleY{
 			c:=avgPixel(img, x, y, scaleX, scaleY)
